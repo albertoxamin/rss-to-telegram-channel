353e227386a8e55a70c6f1c5b468e339db671565
Alberto Xamin <alberto@xamin.it>
2019-06-20 10:36:56 +0200

    another bug-fix

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const telegram = new Telegram(process.env.TG)
 
 let parser = new Parser()
 
+if (!fs.existsSync('lastCheck.txt'))
+	fs.writeFileSync('lastCheck.txt', process.env.STARTING_DATE)
+
 const GetInstantView = (link) => `https://t.me/iv?url=${link}&rhash=${process.env.IV_HASH}`
 const GetLinks = (feedUrl) => {
 	parser.parseURL(feedUrl, function (err, feed) {
 		if (err) {
 			return console.log(err)
 		}
-		var lastDate = (fs.existsSync('lastCheck.txt')) ? new Date(fs.readFileSync('lastCheck.txt').toString()) : undefined
+		var lastDate = (fs.existsSync('lastCheck.txt')) ? new Date(parseInt(fs.readFileSync('lastCheck.txt').toString())) : undefined
 		feed.items.forEach(function (entry) {
 			let m
 			if (lastDate == undefined || new Date(entry.isoDate) > lastDate) {
