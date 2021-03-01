53784c777c98ac02f8b9f6322dcd0d59f8d3ba08
Alberto Xamin <alberto@xamin.it>
2021-03-01 12:13:22 +0100

    update node

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const updateInterval = process.env.UPDATE_INTERVAL ? process.env.UPDATE_INTERVAL
 const { Telegram } = require('telegraf')
 const Parser = require('rss-parser')
 const fs = require('fs')
-
+console.log(process.env.CHANNEL_ID)
 const telegram = new Telegram(process.env.TG)
 
 let parser = new Parser()
 let lastLinks = []
 
 if (!fs.existsSync('lastCheck.txt'))
-	fs.writeFileSync('lastCheck.txt', process.env.STARTING_DATE)
+	fs.writeFileSync('lastCheck.txt', process.env.STARTING_DATE ? process.env.STARTING_DATE : new Date().toISOString())
 
 const GetMessage = (link) => process.env.IV_HASH ? `[️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️](https://t.me/iv?url=${link}&rhash=${process.env.IV_HASH}) ${link}` : link
 const GetLinks = (feedUrl) => {
@@ -17,18 +17,24 @@ const GetLinks = (feedUrl) => {
 		if (err) {
 			return console.log(err)
 		}
-		var lastDate = (fs.existsSync('lastCheck.txt')) ? new Date(parseInt(fs.readFileSync('lastCheck.txt').toString())) : undefined
+		console.log(`[${new Date().toUTCString()}] Parsing RSS`)
+		var lastDate = (fs.existsSync('lastCheck.txt')) ? new Date(fs.readFileSync('lastCheck.txt').toString()) : undefined
+		let skipped = 0
 		feed.items.forEach(function (entry) {
 			let m
+			// console.log(`${new Date(entry.isoDate)} > ${lastDate} --> ${new Date(entry.isoDate) > lastDate}`)
 			if ((lastDate == undefined || new Date(entry.isoDate) > lastDate) && lastLinks.indexOf(entry.link) === -1) {
 				console.log(`[${entry.isoDate}] ${entry.link}`)
 				lastLinks.push(entry.link)
 				if (lastLinks.length > 10)
 					lastLinks.shift() // this should avoid some cases with bad rss feeds
 				PostNews(entry.link)
+			} else {
+				skipped++
 			}
 		})
-		fs.writeFileSync('lastCheck.txt', Date.now())
+		console.log(`[${new Date().toUTCString()}] Done parsing, skipped: ${skipped}`)
+		fs.writeFileSync('lastCheck.txt', new Date().toISOString())
 	})
 }
 
