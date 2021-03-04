1d7eb516367513480a0947a6cd6c8c12be632daf
Alberto Xamin <alberto@xamin.it>
2021-03-04 11:06:47 +0100

    allow multiple RSS links

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const updateInterval = process.env.UPDATE_INTERVAL ? process.env.UPDATE_INTERVAL : 60;
 const { Telegram } = require('telegraf')
 const Parser = require('rss-parser')
-const fs = require('fs')
+const fs = require('fs');
+const { rejects } = require('assert');
 console.log(process.env.CHANNEL_ID)
 const telegram = new Telegram(process.env.TG)
 
@@ -12,37 +13,59 @@ if (!fs.existsSync('lastCheck.txt'))
 	fs.writeFileSync('lastCheck.txt', process.env.STARTING_DATE ? process.env.STARTING_DATE : new Date().toISOString())
 
 const GetMessage = (link) => process.env.IV_HASH ? `[️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️](https://t.me/iv?url=${link}&rhash=${process.env.IV_HASH}) ${link}` : link
-const GetLinks = (feedUrl) => {
-	parser.parseURL(feedUrl, function (err, feed) {
-		if (err) {
-			return console.log(err)
-		}
-		console.log(`[${new Date().toUTCString()}] Parsing RSS`)
-		var lastDate = (fs.existsSync('lastCheck.txt')) ? new Date(fs.readFileSync('lastCheck.txt').toString()) : undefined
-		let skipped = 0
-		feed.items.forEach(function (entry) {
-			let m
-			// console.log(`${new Date(entry.isoDate)} > ${lastDate} --> ${new Date(entry.isoDate) > lastDate}`)
-			if ((lastDate == undefined || new Date(entry.isoDate) > lastDate) && lastLinks.indexOf(entry.link) === -1) {
-				console.log(`[${entry.isoDate}] ${entry.link}`)
-				lastLinks.push(entry.link)
-				if (lastLinks.length > 10)
-					lastLinks.shift() // this should avoid some cases with bad rss feeds
-				PostNews(entry.link)
-			} else {
-				skipped++
+
+const Parse = (feedUrl, lastDate) => {
+	return new Promise((resolve, reject)=> {
+		parser.parseURL(feedUrl, function (err, feed) {
+			if (err) {
+				return console.log(err)
 			}
+			console.log(`[${new Date().toUTCString()}] Parsing ${feedUrl}`)
+			let skipped = 0
+			feed.items.forEach(function (entry) {
+				let m
+				// console.log(`${new Date(entry.isoDate)} > ${lastDate} --> ${new Date(entry.isoDate) > lastDate}`)
+				if ((lastDate == undefined || new Date(entry.isoDate) > lastDate) && lastLinks.indexOf(entry.link) === -1) {
+					console.log(`[${entry.isoDate}] ${entry.link}`)
+					lastLinks.push(entry.link)
+					if (lastLinks.length > 10)
+						lastLinks.shift() // this should avoid some cases with bad rss feeds
+					PostNews(entry.link)
+				} else {
+					skipped++
+				}
+			})
+			console.log(`[${new Date().toUTCString()}] Done parsing ${feedUrl}, skipped: ${skipped}`)
+			resolve()
 		})
-		console.log(`[${new Date().toUTCString()}] Done parsing, skipped: ${skipped}`)
-		fs.writeFileSync('lastCheck.txt', new Date().toISOString())
 	})
 }
 
+const GetLinks = (feedUrl) => {
+	let lastDate = (fs.existsSync('lastCheck.txt')) ? new Date(fs.readFileSync('lastCheck.txt').toString()) : undefined
+	
+	let feeds = feedUrl.split(' ')
+	if (feeds.length == 1) {
+		Parse(feedUrl, lastDate).then(()=>{
+			fs.writeFileSync('lastCheck.txt', new Date().toISOString())
+		})
+	}
+	else {
+		(async () => {
+			while (feeds.length > 0) {
+				await Parse(feeds[0], lastDate)
+				feeds.shift()
+			}
+			fs.writeFileSync('lastCheck.txt', new Date().toISOString())
+		})()
+	}
+}
+
 const PostNews = (link) => {
-	telegram.sendMessage(process.env.CHANNEL_ID, GetMessage(link), Object.assign({ 'parse_mode': 'Markdown' }))
-		.then(message => {
-			console.log(`[${new Date().toUTCString()}] Posted ${link}`)
-		}).catch(err => console.error(err))
+	// telegram.sendMessage(process.env.CHANNEL_ID, GetMessage(link), Object.assign({ 'parse_mode': 'Markdown' }))
+	// 	.then(message => {
+	// 		console.log(`[${new Date().toUTCString()}] Posted ${link}`)
+	// 	}).catch(err => console.error(err))
 }
 
 const FetchPosts = () => {
